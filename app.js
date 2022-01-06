3775a26d72bda2a9958106a2ecff493fed253121
Edward Kaboi <deejaykabwoy@gmail.com>
2022-01-06 21:17:53 +0300

    With Login And Sign Up Authentication

--- app.js
+++ app.js
@@ -2,12 +2,37 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
-
+const session = require("express-session")
+const passport = require("passport")
+const passportLocalMongoose = require("passport-local-mongoose")
 const app = express();
+mongoose.connect("mongodb://localhost/usersDB")
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+    secret:"kaboi",
+    resave:false,
+    saveUninitialized:false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+var userSchema = new mongoose.Schema({
+    username:String,
+    email:String,
+    password:String
+})
+userSchema.plugin(passportLocalMongoose)
+
+
+var User = mongoose.model("User", userSchema)
+
+
+passport.use(User.createStrategy())
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 app.get("/", function(req, res){
     res.render("index");
@@ -23,7 +48,7 @@ app.get("/checkout", (req, res) => {
 
 app.get("/register", (req, res)=>{
 
-    res.render("register")
+   res.render("register")
 })
 
 app.get("/login", (req,res)=>{
@@ -32,7 +57,53 @@ app.get("/login", (req,res)=>{
 })
 app.get("/uploads", (req,res)=>{
 
-    res.render("uploads")
+    if(req.isAuthenticated()){
+
+        res.render("uploads")
+
+    } else{
+
+        res.redirect("/login")
+    }
+
+    
+})
+app.post("/register" , (req,res)=>{
+
+    User.register({username:req.body.username , email:req.body.email} , req.body.password , function(err, user){
+
+        if(err){
+            res.redirect("/register")
+        } else{
+
+            passport.authenticate("local")(req,res, function(){
+
+                res.redirect("/uploads")
+            })
+        }
+    })
+})
+
+app.post("/login", (req,res)=>{
+
+    var user = new User({
+        username:req.body.username,
+        password:req.body.password
+    })
+
+    req.login(user, function(err){
+
+        if(err){
+
+            res.redirect("/login")
+        } else {
+
+            passport.authenticate("local")(req, res , ()=>{
+
+                res.redirect("/uploads")
+            })
+        }
+    })
 })
 
 
